a905d67c7e8272b0276d51d9d533c2c90f10b37b
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-05-27 17:09:44 -0400

    Restructure

--- chapter-4/calculator/step-2/js/index.js
+++ chapter-4/calculator/step-2/js/index.js
@@ -6,19 +6,21 @@ window.onload  = (() => {
   output.value = expression
 
   const clickHandler = ((event) => {
+    let value = event.target.value
+
     /** Write your calculator logic here.
         Use conditionals and math to modify the output variable.
         Use parseInt and a new function, isNaN, to test against value
         to determine what to do.
-
-        Expected things to use:
-          eval() - this is new and should be used sparingly. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
-          isNan() - also new. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-          parseFloat()
-          parseInt()
-          String concatenation
-          Assignment
     */
+
+    if (isNaN(parseFloat(value))) {
+      expression += value
+      console.log(expression)
+    } else {
+      expression += parseInt(value)
+      output.value = expression
+    }
   })
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = clickHandler
