0f2c0404cdb96203343ac092f408f07afaf0b341
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-05-27 16:40:16 -0400

    Chapters 2 and 4

---
+++ chapter-4/solution-code/calculator/js/index.js
@@ -0,0 +1,33 @@
+window.onload  = (() => {
+  const buttons = document.getElementsByTagName('button')
+  let output = document.getElementsByTagName('input')[0]
+  let expression = 0
+
+  output.value = expression
+
+  const clickHandler = ((event) => {
+    let value = event.target.value
+
+    /** Write your calculator logic here.
+        Use conditionals and math to modify the output variable.
+        Use parseInt and a new function, isNaN, to test against value
+        to determine what to do.
+    */
+
+    if (value === '=') {
+      output.value = eval(expression)
+      expression = output.value
+    } else if (value === 'all-clear') {
+      expression = 0
+      output.value = 0
+    } else if (isNaN(parseFloat(value))) {
+      expression += value
+    } else {
+      expression += parseInt(value)
+      output.value = expression
+    }
+  })
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = clickHandler
+  }
+})
---
+++ chapter-4/starter-code/calculator/js/index.js
@@ -0,0 +1,28 @@
+window.onload  = (() => {
+  const buttons = document.getElementsByTagName('button')
+  let output = document.getElementsByTagName('input')[0]
+  let expression = 0
+
+  output.value = expression
+
+  const clickHandler = ((event) => {
+    let value = event.target.value
+
+    /** Write your calculator logic here.
+        Use conditionals and math to modify the output variable.
+        Use parseInt and a new function, isNaN, to test against value
+        to determine what to do.
+
+        Expected things to use:
+          eval() - this is new and should be used sparingly. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
+          isNan() - also new. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+          parseFloat()
+          parseInt()
+          String concatenation
+          Assignment
+    */
+  })
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = clickHandler
+  }
+})
