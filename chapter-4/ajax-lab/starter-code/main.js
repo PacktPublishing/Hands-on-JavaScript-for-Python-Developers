04cef926eaefa8aaabbbd1847b8f007480488a6f
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-23 14:19:23 -0400

    Starter

--- chapter-4/ajax-lab/starter-code/main.js
+++ chapter-4/ajax-lab/starter-code/main.js
@@ -5,5 +5,8 @@ let name = '',
 let hello = "Hello! My name is {name} and I'm from {planet}. I've been in {films} and I'm a Jedi."
 
 fetch('https://swapi.co/api/people/1/')
-    // write your code here.
-})
+  // write your code here.
+  .then((response) => {
+    hello = hello.replace('{name}', name).replace('{planet}', planet).replace('{films}', filmNames)
+    document.querySelector('#main').innerHTML = hello
+  })
