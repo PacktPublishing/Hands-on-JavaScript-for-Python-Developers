83d14520266c0dedeaed6df822a128f5c92fab98
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-23 14:01:14 -0400

    Ajax Lab

---
+++ chapter-4/ajax-lab/starter-code/main.js
@@ -0,0 +1,9 @@
+let name = '',
+  planet = '',
+  films = [],
+  filmNames = ''
+let hello = "Hello! My name is {name} and I'm from {planet}. I've been in {films} and I'm a Jedi."
+
+fetch('https://swapi.co/api/people/1/')
+    // write your code here.
+})
