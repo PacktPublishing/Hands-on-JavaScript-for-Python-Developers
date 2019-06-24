ae33bbf74b9724c09afa6a9ecd758a128be57ae9
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-24 11:16:32 -0400

    Chapter 5

---
+++ chapter-5/alerts-and-prompts/console.js
@@ -0,0 +1,12 @@
+const Officer = function(name, rank, posting) {
+  this.name = name
+  this.rank = rank
+  this.posting = posting
+  this.sayHello = () => {
+  	console.log(this.name)
+  }
+}
+
+const Riker = new Officer("Will Riker", "Commander", "U.S.S. Enterprise")
+
+Riker.sayHello()
