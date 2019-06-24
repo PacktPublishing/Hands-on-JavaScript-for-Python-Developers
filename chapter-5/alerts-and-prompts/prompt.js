25efd6062f3dec98d3840679d68381b9b12b66ef
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-24 11:38:45 -0400

    prompt

---
+++ chapter-5/alerts-and-prompts/prompt.js
@@ -0,0 +1,22 @@
+const Officer = function(name, rank, posting) {
+  this.name = name
+  this.rank = rank
+  this.posting = posting
+
+  this.ask = () => {
+    const values = ['name','rank','posting']
+
+    let answer = prompt("What would you like to know about this officer?")
+    answer = answer.toLowerCase()
+
+    if (values.indexOf(answer) < 0) {
+      alert('Value not found')
+    } else {
+      alert(this[answer])
+    }
+  }
+}
+
+const Riker = new Officer("Will Riker", "Commander", "U.S.S. Enterprise")
+
+Riker.ask()
