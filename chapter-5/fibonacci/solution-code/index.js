0e28a7c9223f55cacdeb3eec073f9d5d1c5095f1
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-26 10:08:23 -0400

    Fibonacci

---
+++ chapter-5/fibonacci/solution-code/index.js
@@ -0,0 +1,15 @@
+function fibonacci(num){
+  let a = 1, b = 0, temp
+
+  while (num >= 0){
+    temp = a
+    a = a + b
+    b = temp
+    num--
+  }
+
+  return b
+}
+
+const response = prompt("How many numbers?")
+alert(`The Fibonacci number is ${fibonacci(response)}`)
---
+++ chapter-5/fibonacci/starter-code/index.js
@@ -0,0 +1,7 @@
+function fibonacci(num){
+  // write your  code here
+}
+
+const response = prompt("How many numbers?")
+
+// how do you deal with calling the function and its output?
