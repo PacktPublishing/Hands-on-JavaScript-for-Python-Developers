a3e31c35d2a5c16f6cb9e4af11c041ada22e2d3a
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-06-26 10:37:40 -0400

    Checking numbers

--- chapter-5/fibonacci/solution-code-number-check/index.js
+++ chapter-5/fibonacci/solution-code-number-check/index.js
@@ -12,4 +12,8 @@ function fibonacci(num){
 }
 
 let response = prompt("How many numbers?")
+while (typeof(parseInt(response)) !== "number" || !Number.isInteger(parseFloat(response))) {
+  response = prompt("Please enter an integer:")
+}
+
 alert(`The Fibonacci number is ${fibonacci(response)}`)
--- chapter-5/fibonacci/starter-code/index.js
+++ chapter-5/fibonacci/starter-code/index.js
@@ -2,6 +2,6 @@ function fibonacci(num){
   // write your  code here
 }
 
-const response = prompt("How many numbers?")
+let response = prompt("How many numbers?")
 
 // how do you deal with calling the function and its output?
