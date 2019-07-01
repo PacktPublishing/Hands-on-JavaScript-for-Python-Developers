123dcad9690a633925e1c59ee6b9c4324c1e8932
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-07-01 19:45:48 -0400

    Matrix problem

---
+++ chapter-5/matrix/solution-code/index.js
@@ -0,0 +1,40 @@
+const a = [1, 3, 5, 7, 9]
+const b = [2, 5, 7, 9, 14]
+
+// compute the products of each permutation for efficient retrieval
+
+const products = { }
+
+const makeProducts = function(array1, array2) {
+  array1.forEach( (multiplicant) => {
+    array2.forEach( (multiplier) => {
+      products[`${multiplicant}-${multiplier}`] = multiplicant * multiplier
+    })
+  })
+}
+
+const getProducts = function(a,b) {
+  // make an efficient means of retrieval
+  return products[`${a}-${b}`] || null
+}
+
+makeProducts(a,b)
+
+console.log(getProducts(a[4],b[0]))  // 18
+console.log(getProducts(a[0],b[0]))  // 2
+console.log(getProducts(a[1],b[3]))  // 27
+console.log(getProducts(a[4],b[2]))  // 63
+console.log(getProducts(a[10],b[2]))  // null
+
+// bonus: get an arbitrary key/value pair. If nonexistent, compute it and store it.
+
+const getOrMakeProducts = function(a,b) {
+  if (!products[`${a}-${b}`]) {
+    makeProducts([a],[b])
+  }
+  return products[`${a}-${b}`]
+}
+
+
+console.log(getOrMakeProducts(1100,11234)) // 12357400
+console.log(getProducts(1100,11234)) // 12357400
---
+++ chapter-5/matrix/starter-code/index.js
@@ -0,0 +1,10 @@
+const a = [1, 3, 5, 7, 9]
+const b = [2, 5, 7, 9, 14]
+
+// compute the products of each permutation for efficient retrieval
+
+const getProducts = function(a,b) {
+  // make an efficient means of retrieval
+}
+
+// bonus: get an arbitrary key/value pair. If nonexistent, compute it and store it.
