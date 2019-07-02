bf5b6969980c9b0448d37bd29aabb765eba2e635
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-07-02 09:32:25 -0400

    Matrix problem

--- chapter-5/matrix/solution-code/index.js
+++ chapter-5/matrix/solution-code/index.js
@@ -7,15 +7,25 @@ const products = { }
 
 const makeProducts = function(array1, array2) {
   array1.forEach( (multiplicant) => {
+    if (!products[multiplicant]) {
+      products[multiplicant] = { }
+    }
     array2.forEach( (multiplier) => {
-      products[`${multiplicant}-${multiplier}`] = multiplicant * multiplier
+      if (!products[multiplier]) {
+        products[multiplier] = { }
+      }
+      products[multiplicant][multiplier] = multiplicant * multiplier
+      products[multiplier][multiplicant] = multiplicant * multiplier
     })
   })
 }
 
 const getProducts = function(a,b) {
   // make an efficient means of retrieval
-  return products[`${a}-${b}`] || null
+  if (products[a]) {
+    return products[a][b] || null
+  }
+  return null
 }
 
 makeProducts(a,b)
@@ -29,12 +39,13 @@ console.log(getProducts(a[10],b[2]))  // null
 // bonus: get an arbitrary key/value pair. If nonexistent, compute it and store it.
 
 const getOrMakeProducts = function(a,b) {
-  if (!products[`${a}-${b}`]) {
+  if (!products[a] || !products[a][b]) {
     makeProducts([a],[b])
   }
-  return products[`${a}-${b}`]
+  return products[a][b]
 }
 
 
 console.log(getOrMakeProducts(1100,11234)) // 12357400
 console.log(getProducts(1100,11234)) // 12357400
+console.log(getProducts(11234,1100)) // 12357400
