962fed91bd63d9accd440e92efd9de53f6a4832c
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-07-07 14:47:44 -0400

    Stickies

---
+++ chapter-6/stickies/solution-code/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container') // set .container to a variable so we don't need to find it every time we click
+let noteCount = 1 // inital value
+
+// access our button and assign a click handler
+document.querySelector('.box-creator-button').addEventListener('click', () => {
+  //  create our DOM element
+  const stickyNote = document.createElement('div')
+
+  // set our class name
+  stickyNote.className = 'box'
+
+  // get our other DOM elements
+  const stickyMessage = document.querySelector('.box-color-note')
+  const stickyColor = document.querySelector('.box-color-input')
+
+  // get our variables
+  const message = stickyMessage.value
+  const color = stickyColor.value
+
+  // blank out the input fields
+  stickyMessage.value =  stickyColor.value = ''
+
+  // define the attributes
+  stickyNote.innerHTML = `${noteCount++}. ${message}`
+  stickyNote.style.backgroundColor = color
+
+  // add the sticky
+  container.appendChild(stickyNote)
+})
---
+++ chapter-6/stickies/starter-code/script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container') // set .container to a variable so we don't need to find it every time we click
+let noteCount = 1 // inital value
+
+// access our button and assign a click handler
+
+  //  create our DOM element
+
+  // set our class name
+
+  // get our other DOM elements
+
+  // get our variables
+
+  // blank out the input fields
+
+  // define the attributes
+
+  // add the sticky
