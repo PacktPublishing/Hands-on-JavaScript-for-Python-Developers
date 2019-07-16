a2a009b3eedac6f50b9496d89bd9348ccc0728d4
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2019-07-16 13:10:08 -0400

    Starter

--- chapter-7/pokeapi/starter-code/main.js
+++ chapter-7/pokeapi/starter-code/main.js
@@ -1,97 +1,51 @@
+/**
+  * Let's use the `class` structure! It's pretty similar to what you've probably seen in other languages. Classes are new to ES6, by the way.
+  */
+  s
 class Poke {
   constructor() {
-    this.loader = document.querySelector('#loader')
-    this.chosenPokemon = {}
-    this.Player1 = {}
-    this.Player2 = {}
-
-    document.addEventListener('click', (e) => {
-      if (e.target.className == 'go') {
-        this.choosePokemon(e.target.parentNode.querySelector('.pokeSelector').value, e.target.parentNode)
-      }
-    })
+    /**
+      * Use the constructor as you would in other languages: Set up your instance variables and globals
+      */
   }
 
   getPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1000')
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        const pokeSelector = document.querySelector('.pokeSelector.main')
-
-        data.results.forEach((poke) => {
-          const option = document.createElement('option')
-          option.value = poke.url
-          option.innerHTML = poke.name
-          pokeSelector.appendChild(option)
-        })
-
-        const selector = pokeSelector.cloneNode(true)
-        document.querySelector('.pokeSelector.clone').replaceWith(selector)
-
-        this.toggleLoader()
-
-        document.querySelector('#Player1').style.visibility = 'visible'
-        document.querySelector('#Player2').style.visibility = 'visible'
-      })
+    /**
+      * First, we need to get all of our Pokémon so that we can add them to tthe <select> list in order to choose them.
+      * Hint: The URL you can use to get all of them in the API without pagination is: https://pokeapi.co/api/v2/pokemon?limit=1000
+      * You'll probably want to do something with the loader div as well.
+      */
   }
 
   choosePokemon(url, parent) {
-    this.toggleLoader()
-    fetch(url)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        this.toggleLoader()
+    /**
+      * Next, we want to enable the player to select a pokémon.
+      * After retrieving the information from the API, peruse the response data. You'll see quite a lot of information.
+      * To help you sift through the data for the each pokémon's basic moves, I've left you the below code.
+      * Look up the Array.filter method here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+      */
 
-        const moves = data.moves.filter((move) => {
-          const mymoves = move.version_group_details.filter((level) => {
-            return level.level_learned_at === 1
-          })
-          return mymoves.length > 0
+      const moves = data.moves.filter((move) => {
+        const mymoves = move.version_group_details.filter((level) => {
+          return level.level_learned_at === 1
         })
-
-        this.chosenPokemon[parent.id] = data
-        this.populateCard(data, parent, moves)
+        return mymoves.length > 0
       })
   }
 
   populateCard(data, root, moves) {
-    const card = root.querySelector('.card')
-    card.querySelector('img').setAttribute('src', data.sprites.front_default)
-    card.querySelector('h3').innerHTML = data.name
-
-    document.addEventListener('hp', (e) => {
-      if (e.detail.player === root.id) {
-        card.querySelector('.hp').innerHTML = e.detail.hp
-      }
-
-    })
-
-    card.querySelector('.move1').innerHTML = moves[0].move.name
-    card.querySelector('.move2').innerHTML = moves[1].move.name
-
-    card.querySelector('.move1').dataset.url = moves[0].move.url
-    card.querySelector('.move2').dataset.url = moves[1].move.url
-
-    card.addEventListener('click', (e) => {
-      if (e.target.nodeName === 'BUTTON') {
-        this.doMove(e.target.dataset.url, root.id)
-      }
-    })
-
-    this.hp = {
-      player: root.id,
-      hp: data.stats[5].base_stat
-    }
-
-    card.style.visibility = 'visible'
-
+    /**
+      * Be sure you're keeping track of the player! That's where this function's `root` comes into play.
+      * Now that you've passed the pokémon's data to this function, build out the card to display its stats.
+      * Reference the HTML and CSS to be sure you have the right pieces.
+      */
   }
 
   set hp(event) {
+    /**
+      * This is an example of a setter in a JavaScript class. I'll leave it here for you in its entirety to dissect.
+      */
+
     if (event.hp) {
       this[event.player].hp = event.hp
     }
@@ -109,28 +63,9 @@ class Poke {
   }
 
   doMove(url, player) {
-    const opponent = (player == 'Player1') ? 'Player2' : 'Player1'
-
-    fetch(url)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        if (data.power) {
-          const damage = Math.floor(Math.random() * data.power)
-          this.hp = {
-            player: opponent,
-            damage: damage
-          }
-          if (this[opponent].hp <= 0) {
-            this.declareWinner(opponent)
-          } else {
-            this.doResult("It's effective!", true)
-          }
-        } else {
-          this.doResult("It's not very effective...", true)
-        }
-      })
+    /**
+      *  How would we play a move against the other player?
+      */
   }
 
   declareWinner(player) {
@@ -138,6 +73,9 @@ class Poke {
   }
 
   doResult(data, fade = false) {
+    /**
+      * As this is visual logic, here's the complete code for this function
+      */
     const result = document.querySelector('#result')
     result.querySelector('h2').innerHTML = data
     result.style.visibility = 'visible'
@@ -163,6 +101,9 @@ class Poke {
   }
 
   toggleLoader() {
+    /**
+      * As this is visual logic, here's the complete code for this function
+      */
     if (this.loader.style.visibility === 'visible' || this.loader.style.visibility === '') {
       this.loader.style.visibility = 'hidden'
     } else {
@@ -171,4 +112,5 @@ class Poke {
   }
 }
 
+// Here is how we create an instance of our claass and give it life by invoking a method.
 const p = new Poke().getPokemon()
