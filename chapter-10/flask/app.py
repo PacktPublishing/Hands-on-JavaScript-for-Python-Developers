344333960865d61a7205c908475f33366156a576
Sonyl Nagale <sonyl@nagale.com>
2019-12-26 11:01:15 -0600

    WIP

--- chapter-10/flask/app.py
+++ chapter-10/flask/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import time
 
 # App config.
@@ -9,23 +8,19 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
 
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
 
-    @app.route("/", methods=['GET', 'POST'])
-    def hello():
-        now = time.time()
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    now = time.time()
 
-        form = ReusableForm(request.form)
+    if request.method == 'POST':
+        name = request.form['name']
+        if name == '':
+            flash("You didn't enter your name!")
+        else:
+            flash("Hello, %s" % name)
 
-        if request.method == 'POST':
-            name = request.form['name']
-            if name == '':
-                flash("You didn't enter your name!")
-            else:
-                flash("Hello, %s" % name)
-  
-        return render_template('hello.html', form=form, now=now)
+    return render_template('hello.html', form=request.form, now=now)
 
 if __name__ == "__main__":
     app.run()
