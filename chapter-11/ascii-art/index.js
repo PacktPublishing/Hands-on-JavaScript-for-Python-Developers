dd433f814ad834847d1b685c02ffdb32b5dba980
Sonyl Nagale <sonyl@nagale.com>
2020-01-04 09:47:58 -0500

    Punctuation

--- chapter-11/ascii-art/index.js
+++ chapter-11/ascii-art/index.js
@@ -1,5 +1,5 @@
 const ImageToAscii = require('image-to-ascii')
 
 ImageToAscii(__dirname + '/image.jpg', (err, converted) => {
-    console.log(err || converted);
-});
+    console.log(err || converted)
+})
