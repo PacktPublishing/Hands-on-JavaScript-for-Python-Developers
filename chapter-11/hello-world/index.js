6995db7b25e140b3b8f58e7ffa2acc4f16dbd048
Sonyl Nagale <sonyl@nagale.com>
2020-01-04 11:22:09 -0500

    WIP Hello World

--- chapter-11/hello-world/index.js
+++ chapter-11/hello-world/index.js
@@ -1,6 +1,24 @@
 const http = require('http')
+const ImageToAscii = require('image-to-ascii')
+const Convert = require('ansi-to-html')
+const convert = new Convert()
+
+const css = `
+<style>
+body {
+  background-color: #000;
+}
+* {
+  font-family: "Courier New";
+  white-space: pre-wrap;
+}
+</style>
+`
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end("Hello, World!")
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  ImageToAscii(__dirname + '/img/image.jpg', (err, converted) => {
+    res.write(css)
+    res.end(convert.toHtml(err || converted))
+  })
 }).listen(8080)
