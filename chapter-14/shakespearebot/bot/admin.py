7f6ba17802964a484993dc3a1d12538682157780
Sonyl Nagale <sonyl@nagale.com>
2020-02-29 08:08:52 -0800

    Parity

--- chapter-14/shakespearebot/bot/admin.py
+++ chapter-14/shakespearebot/bot/admin.py
@@ -1,5 +1,7 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
-# Register your models here.
-
 from .models import Text
-admin.site.register(Text)
+
+@admin.register(Text)
+class TextAdmin(ImportExportModelAdmin):
+   pass
--- chapter-14/shakespearebot/shakespearebot/settings.py
+++ chapter-14/shakespearebot/shakespearebot/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
