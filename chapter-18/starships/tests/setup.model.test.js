1c2fd6036a903eb175e93faf7f8cffe12cc0b025
Sonyl Nagale <sonyl@nagale.com>
2020-04-19 15:28:57 -0400

    Cleanup

--- chapter-18/starships/tests/setup.model.test.js
+++ chapter-18/starships/tests/setup.model.test.js
@@ -2,27 +2,23 @@ const MongoDB = require('../models/mongo')
 const insertRandomNames = require('../models/setup')
 
 describe('insert', () => {
-  let db;
+  let db
 
   beforeAll(async () => {
     db = await MongoDB.connectDB('test')
-  });
+  })
 
   afterAll(async (done) => {
     await db.collection('names').deleteMany({})
     await MongoDB.disconnectDB()
     done()
-  });
-
-  beforeEach(async () => {
-    
-  });
+  })
 
   it('should insert the random names', async () => {
     await insertRandomNames()
 
     const names = await db.collection("names").find().toArray()
     expect(names.length).toBeGreaterThan(0)
-  });
+  })
 
-});
+})
