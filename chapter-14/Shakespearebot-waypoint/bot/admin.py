530b7a8f6d83434b87b71054570cfcc84288cdac
Sonyl Nagale <sonyl@nagale.com>
2020-06-27 15:11:38 -0400

    Adding bot waypoint

---
+++ chapter-14/Shakespearebot-waypoint/bot/admin.py
@@ -0,0 +1,8 @@
+from import_export.admin import ImportExportModelAdmin 
+from django.contrib import admin
+from .models import Text
+
+@admin.register(Text)
+
+class TextAdmin(ImportExportModelAdmin):
+  pass
